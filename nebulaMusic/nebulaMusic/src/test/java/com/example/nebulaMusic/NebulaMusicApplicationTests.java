0d2cb636621d660100084ba0b2b78d6579be8399
sansanty03 <138645645+sansanty03@users.noreply.github.com>
2026-09-10 19:16:06 -0700

    nebula

---
+++ nebulaMusic/nebulaMusic/src/test/java/com/example/nebulaMusic/NebulaMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nebulaMusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NebulaMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
